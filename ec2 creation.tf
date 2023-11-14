344856e6c490d23c0e36519c6908fe2e21e9356c
vagdevi-123 <94599331+vagdevi-123@users.noreply.github.com>
2023-11-14 15:46:03 +0530

    Update ec2 creation.tf

--- ec2 creation.tf
+++ ec2 creation.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "hello-world" {
 
  ami = "ami-02e94b011299ef128" 
  instance_type = "t2.micro"
- key_name = "terraform"
+ key_name = "instance1kp"
  tags = {
 	 Name = "Hello world"
   project="terrafoem-cloud"
